fbfb2f7826bfd2943ca0c38e0a9de3b86266c044
Rafael-N-Moura <rafaelnm.dev@gmail.com>
2022-03-25 15:48:12 -0300

    🚧 Starting Get All Ativos by Estabelecimento Usecase and Test

---
+++ lib/features/fake_way/domain/usecases/get_all_ativos_by_estabelecimento_from_api_usecase.dart
@@ -0,0 +1,6 @@
+import 'package:fake_way/core/errors/failures.dart';
+import 'package:dartz/dartz.dart';
+import 'package:fake_way/core/usecases/usecase.dart';
+import 'package:fake_way/features/fake_way/domain/entities/estabelecimento_entity.dart';
+
+import '../entities/ativo_entity.dart';
