2b87e1e0bf8f9b19aa6d549c3db010e8ca7c9c6f
Rafael-N-Moura <rafaelnm.dev@gmail.com>
2022-03-25 14:42:54 -0300

    🚧 estabelecimento entity added

---
+++ lib/features/fake_way/domain/entities/estabelecimento_entity.dart
@@ -0,0 +1,20 @@
+import 'package:equatable/equatable.dart';
+
+class Estabelecimento extends Equatable {
+  final int companyId;
+  final String companyName;
+  final List<Ativo> ativos;
+
+  const Estabelecimento({
+    required this.companyId,
+    required this.companyName,
+    required this.ativos,
+  });
+
+  @override
+  List<Object?> get props => [
+        companyId,
+        companyName,
+        ativos,
+      ];
+}
