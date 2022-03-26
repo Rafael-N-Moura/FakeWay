4c83234c54113ef1694d08312e3446fb7800434d
Gustavo Neri Pereira Dias <gutneri@hotmail.com>
2022-03-25 21:53:57 -0300

    ✅ Get all estabelecimentos usecase and tests completed

---
+++ lib/features/fake_way/domain/usecases/get_all_estabelecimentos_usecase.dart
@@ -0,0 +1,16 @@
+import 'package:fake_way/core/errors/failures.dart';
+import 'package:dartz/dartz.dart';
+import 'package:fake_way/core/usecases/usecase.dart';
+import 'package:fake_way/features/fake_way/domain/entities/estabelecimento_entity.dart';
+import 'package:fake_way/features/fake_way/domain/repositories/i_estabelecimento_repository.dart';
+
+class GetAllEstabelecimentosUsecase
+    implements UseCase<List<Estabelecimento>, NoParams> {
+  final IEstabelecimentoRepository repository;
+
+  GetAllEstabelecimentosUsecase(this.repository);
+  @override
+  Future<Either<Failure, List<Estabelecimento>>> call(NoParams params) async {
+    return repository.getAllEstabelecimentos();
+  }
+}
