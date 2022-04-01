3c02c1221e8598c5ef91538875c18a9241941c4e
Gustavo Neri Pereira Dias <gutneri@hotmail.com>
2022-04-01 17:59:50 -0300

    🚧 work on estabelecimento presentation page

--- lib/features/estabelecimento_feature/presenter/widgets/custom_app_bar_widget.dart
+++ lib/features/estabelecimento_feature/presenter/widgets/custom_app_bar_widget.dart
@@ -1,26 +1,23 @@
 import 'package:fake_way/core/utils/way_colors.dart';
 import 'package:flutter/material.dart';
 
-class CustomAppBar extends StatelessWidget {
-  const CustomAppBar({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return AppBar(
-      title: const Text("Selecione a Empresa"),
-      titleTextStyle: TextStyle(
-          color: WayColors.grey,
-          fontSize: 20,
-          height: 20,
-          fontWeight: FontWeight.w500),
-      actions: [
-        IconButton(
-            onPressed: () {},
-            icon: Icon(
-              Icons.align_horizontal_right_rounded,
-              color: WayColors.primaryColor,
-            ))
-      ],
-    );
-  }
+class CustomAppBarWidget extends AppBar {
+  CustomAppBarWidget({Key? key})
+      : super(
+          key: key,
+          title: const Text("Selecione a Empresa"),
+          titleSpacing: 24,
+          backgroundColor: Colors.white,
+          elevation: 0,
+          titleTextStyle: TextStyle(
+              color: WayColors.grey, fontSize: 20, fontWeight: FontWeight.w500),
+          actions: [
+            IconButton(
+                onPressed: () {},
+                icon: Icon(
+                  Icons.align_horizontal_right_rounded,
+                  color: WayColors.primaryColor,
+                ))
+          ],
+        );
 }
---
+++ lib/features/estabelecimento_feature/presenter/widgets/estabelecimento_card_widget.dart
@@ -0,0 +1,45 @@
+import 'package:fake_way/core/utils/way_colors.dart';
+import 'package:fake_way/features/estabelecimento_feature/domain/entities/estabelecimento_entity.dart';
+import 'package:flutter/material.dart';
+
+class EstabelecimentoCardWidget extends StatelessWidget {
+  final Estabelecimento estabelecimento;
+  const EstabelecimentoCardWidget({Key? key, required this.estabelecimento})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      child: Container(
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(12),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.grey.withOpacity(0.5),
+              spreadRadius: 1,
+              blurRadius: 3,
+              offset: const Offset(0, 3),
+            ),
+          ],
+        ),
+        margin: const EdgeInsets.symmetric(vertical: 12, horizontal: 24),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 24),
+          child:
+              Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
+            Text(
+              estabelecimento.companyName,
+              style: const TextStyle(fontSize: 16, fontWeight: FontWeight.w500),
+            ),
+            Icon(
+              Icons.arrow_forward_ios,
+              color: WayColors.primaryColor,
+            )
+          ]),
+        ),
+      ),
+      onTap: () {},
+    );
+  }
+}
