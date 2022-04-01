00a3044ed94b52efcd661d705fbe41ead63e190e
Rafael-N-Moura <rafaelnm.dev@gmail.com>
2022-03-31 22:50:19 -0300

    🔧 Colors

---
+++ lib/core/utils/colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class Colors {
+  static Color primaryColor = const Color(0xFF004586);
+  static Color grey = const Color(0xFFAAAFB9);
+  static Color greyLight = const Color(0xFFE9E9E9);
+  static Color primaryLight = const Color(0xFFDFEAF5);
+  static Color black = const Color(0xFF1D1F23);
+}
