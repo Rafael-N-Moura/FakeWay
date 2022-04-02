f3f1fe449d25a6da3019edf9fc8b3e87f7bd0b36
Rafael-N-Moura <rafaelnm.dev@gmail.com>
2022-04-01 21:12:32 -0300

    🔧 Back to ModularApp in main file

--- lib/app_widget.dart
+++ lib/app_widget.dart
@@ -1,3 +1,4 @@
+import 'package:fake_way/core/utils/way_colors.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 
@@ -10,6 +11,8 @@ class AppWidget extends StatelessWidget {
       title: 'FakeWay',
       routeInformationParser: Modular.routeInformationParser,
       routerDelegate: Modular.routerDelegate,
+      theme: ThemeData(
+          primaryColor: WayColors.primaryColor, fontFamily: 'Raleway'),
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,3 @@
-import 'package:fake_way/core/utils/colors.dart';
-import 'package:fake_way/features/enviar_dato_feature.dart/presentation/pages/enviar_dado_page.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 
@@ -8,14 +6,9 @@ import 'app_widget.dart';
 
 void main() {
   runApp(
-    // ModularApp(
-    //   module: AppModule(),
-    //   child: const AppWidget(),
-    // ),
-    MaterialApp(
-      theme: ThemeData(
-          primaryColor: CustomColors.primaryColor, fontFamily: 'Raleway'),
-      home: EnviarDadoPage(),
+    ModularApp(
+      module: AppModule(),
+      child: const AppWidget(),
     ),
   );
 }
